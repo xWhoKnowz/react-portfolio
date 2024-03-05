39424945467645a42da0d81178be0357af13ce27
xwhoknowz <jeremiah2498@gmail.com>
2024-03-05 02:19:50 -0600

    started form on contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,19 @@
 export default function Contact() {
     
     return (
-        <section></section>
+        <main className="contact">
+            <div className="mb-3">
+            <label for="name" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+            </div>
+            <div className="mb-3">
+            <label for="email" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+            </div>
+            <div className="mb-3">
+            <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+        </main>
     )
 }
