01df6e03d4c4c5dbb11b0ad186cbb046cbebe14b
xwhoknowz <jeremiah2498@gmail.com>
2024-03-06 19:43:08 -0600

    finished error page and added repo links to portfolio page

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,15 +1,24 @@
+import github from '../../assets/github.png'
+
 export default function Project(props) {
   console.log(props);
 
   return (
+    <>
     <section className="projectCard col-sm-12 col-md-12 col-lg-6">
       <h2 className="projectTitle">{props.project.title}</h2>
+
       <a href={props.project.link}>
         <img
           className="project"          
           src={props.project.img}
         />
       </a>
+      <a href={props.project.repo}>
+        <img className='github' src={github}/>
+      </a>
     </section>
+    
+    </>
   );
 }
--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -5,6 +5,7 @@ export default function Error() {
     return (
         <main>
             <img className='errorPage' src={error}/>
+            <h2 className='errorMessage'>Error 404: This is not the page you are looking for.</h2>
         </main>
     )
 }
