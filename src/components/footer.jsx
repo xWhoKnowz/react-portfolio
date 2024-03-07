7815ec0196cc376b6cf3d3bfbc55252da9387242
xwhoknowz <jeremiah2498@gmail.com>
2024-03-06 19:28:19 -0600

    created footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,21 @@
+import github from '../../assets/github.png'
+import linkedin from '../../assets/linkedin.png'
+import formerlyTwitter from '../../assets/x.png'
+
+
 export default function Footer() {
     
     return (
-        <section></section>
+        <footer className='d-flex justify-content-center'>
+            <a href='https://github.com/xWhoKnowz'>
+            <img className='linkLogo' src={github}/>
+            </a>
+            <a href='https://www.linkedin.com/'>
+            <img className='linkedin linkLogo' src={linkedin}/>
+            </a>
+            <a href='https://twitter.com/'>
+            <img className='linkLogo' src={formerlyTwitter}/>
+            </a>
+        </footer>
     )
 }
