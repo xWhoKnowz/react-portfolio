7cc2570b6faf30b874a1670bcd220788291ee5bc
xwhoknowz <jeremiah2498@gmail.com>
2024-03-06 21:20:11 -0600

    fixed form responsiveness

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -7,7 +7,6 @@ export default function Project(props) {
     <>
     <section className="projectCard col-sm-12 col-md-12 col-lg-6">
       <h2 className="projectTitle">{props.project.title}</h2>
-
       <a href={props.project.link}>
         <img
           className="project"          
